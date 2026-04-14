789af10309a9c99812466b2111ec1207aec53d21
Carlos Eduardo Oliveira Dos Santos <oliveira.santos.carlos2202@escola.pr.gov.br>
2026-04-14 08:54:27 -0300

    14-4

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  noStroke();
+
+  //azul
+  fill(0,85,164);
+  rect(0,0,200,400);
+  
+  //branco
+  fill(255);
+  rect(200,0,200,400);
+  
+  //vermelho
+  fill(239,65,53);
+  rect(400,0,200,400);
+}  
